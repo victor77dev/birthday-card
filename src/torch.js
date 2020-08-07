da3db5adde09ac613ec2c260f0b7ff65b656f03c
victor77dev <victor77.dev@gmail.com>
2020-08-07 22:34:43 +0200

    Remove unnecessary check

--- src/torch.js
+++ src/torch.js
@@ -6,38 +6,24 @@ const turnOn = () => {
         console.error('mediaDevices is not supported.');
         return null;
     }
-    const {mediaDevices} = navigator;
 
-    //Get the environment camera (usually the second one)
-    mediaDevices.enumerateDevices().then(devices => {
-        const cameras = devices.filter(
-            (device) => device.kind === 'videoinput'
-        );
-        if (cameras.length === 0) {
-            throw 'No camera found on this device.';
-        }
-        const camera = cameras[cameras.length - 1];
+    navigator.mediaDevices.getUserMedia({
+        video: {
+            facingMode: {exact: 'environment'},
+            height: {ideal: 720},
+            width: {ideal: 1280},
+        },
+    }).then((stream) => {
 
-        mediaDevices.getUserMedia({
-            video: {
-                deviceId: camera.deviceId,
-                facingMode: ['environment'],
-                height: {ideal: 720},
-                width: {ideal: 1280},
-            }
-        }).then((stream) => {
-            const track = stream.getVideoTracks()[0];
-            //Create image capture object and get camera capabilities
-            const imageCapture = new ImageCapture(track)
-            imageCapture.getPhotoCapabilities().then(() => {
-                //todo: check if camera has a torch
-                track.applyConstraints({
-                    advanced: [{torch: true}]
-                }).then(() => {
-                    console.log('start torch success')
-                }).catch((error) => {
-                    console.log('applyConstraints error: ' + JSON.stringify(error.message))
-                });
+        const track = stream.getVideoTracks()[0];
+        const imageCapture = new ImageCapture(track)
+        imageCapture.getPhotoCapabilities().then(() => {
+            track.applyConstraints({
+                advanced: [{torch: true}]
+            }).then(() => {
+                console.log('start torch success')
+            }).catch((error) => {
+                console.log('applyConstraints error: ' + JSON.stringify(error.message))
             });
         });
     });
