9e00fb7a0c7bb9e24184650132b2c68b469aefdd
victor77dev <victor77.dev@gmail.com>
2020-08-17 00:08:50 +0200

    Remove previous page

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,8 @@
 const app = document.querySelector('#app');
 export function goTo(page) {
+    const pages = document.querySelectorAll('.page');
+    pages && pages.forEach((page) => page.remove());
+
     app.insertAdjacentHTML('afterbegin', page.element);
     page?.init();
 }
