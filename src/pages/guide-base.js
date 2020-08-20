aefa2146c1850f5864fd0e048289bae314f8ac62
victor77dev <victor77.dev@gmail.com>
2020-08-20 21:13:32 +0200

    Add GuideBase page

--- src/pages/guide-base.js
+++ src/pages/guide-base.js
@@ -1,17 +1,17 @@
-import {GuideBase} from './guide-base.js';
+import {Start} from './start.js';
 import * as Utils from '../utils.js';
 
-export class CheckParts {
+export class GuideBase {
     init = () => {
         const next = document.querySelector('#next');
         next.addEventListener('click', function() {
-            Utils.goTo(new GuideBase());
+            Utils.goTo(new Start());
         });
     };
 
     element =
     '<div class="page">\
-        <h1>Make sure you have all the parts!<h1>\
+        <h1>Assemble 4 triangles base<h1>\
         <button id="next">Next</button>\
     </div>';
 }
