25b12584d0110170025c80299fafaecd7cf150b3
victor77dev <victor77.dev@gmail.com>
2020-08-20 21:30:50 +0200

    Add GuideCard page

--- src/pages/guide-card.js
+++ src/pages/guide-card.js
@@ -1,17 +1,17 @@
-import {GuideCard} from './guide-card.js';
+import {Start} from './start.js';
 import * as Utils from '../utils.js';
 
-export class GuideBase {
+export class GuideCard {
     init = () => {
         const next = document.querySelector('#next');
         next.addEventListener('click', function() {
-            Utils.goTo(new GuideCard());
+            Utils.goTo(new Start());
         });
     };
 
     element =
     '<div class="page">\
-        <h1>Assemble 4 triangles base<h1>\
+        <h1>Put card for phone base.<h1>\
         <button id="next">Next</button>\
     </div>';
 }
