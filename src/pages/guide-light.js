652619c99c3e856bd10e1d06edf97d0e588d88e8
victor77dev <victor77.dev@gmail.com>
2020-08-21 21:12:10 +0200

    Add GuideLight page

--- src/pages/guide-light.js
+++ src/pages/guide-light.js
@@ -1,17 +1,17 @@
-import {GuideLight} from './guide-light.js';
+import {Start} from './start.js';
 import * as Utils from '../utils.js';
 
-export class GuideCard {
+export class GuideLight {
     init = () => {
         const next = document.querySelector('#next');
         next.addEventListener('click', function() {
-            Utils.goTo(new GuideLight());
+            Utils.goTo(new Start());
         });
     };
 
     element =
     '<div class="page">\
-        <h1>Put card for phone base.<h1>\
+        <h1>Put reflect card under base.<h1>\
         <button id="next">Next</button>\
     </div>';
 }
