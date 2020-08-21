69095f450c10d7b0bb889cfa23a458da08a501ed
victor77dev <victor77.dev@gmail.com>
2020-08-21 21:34:49 +0200

    Add GuideProject page

--- src/pages/guide-project.js
+++ src/pages/guide-project.js
@@ -1,17 +1,18 @@
-import {GuideProject} from './guide-project.js';
+import {Start} from './start.js';
 import * as Utils from '../utils.js';
 
-export class GuideMoon {
+export class GuideProject {
     init = () => {
         const next = document.querySelector('#next');
         next.addEventListener('click', function() {
-            Utils.goTo(new GuideProject());
+            Utils.goTo(new Start());
         });
     };
 
     element =
     '<div class="page">\
-        <h1>Put star moon card.<h1>\
+        <h1>Put project card.<h1>\
         <button id="next">Next</button>\
     </div>';
 }
+
