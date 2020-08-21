b257dbe8f76f18d2b63ae4dd7e2f5ad03af8fe6b
victor77dev <victor77.dev@gmail.com>
2020-08-21 21:17:14 +0200

    Add GuideMoon page

--- src/pages/guide-moon.js
+++ src/pages/guide-moon.js
@@ -1,17 +1,17 @@
-import {GuideMoon} from './guide-moon.js';
+import {Start} from './start.js';
 import * as Utils from '../utils.js';
 
-export class GuideLight {
+export class GuideMoon {
     init = () => {
         const next = document.querySelector('#next');
         next.addEventListener('click', function() {
-            Utils.goTo(new GuideMoon());
+            Utils.goTo(new Start());
         });
     };
 
     element =
     '<div class="page">\
-        <h1>Put reflect card under base.<h1>\
+        <h1>Put star moon card.<h1>\
         <button id="next">Next</button>\
     </div>';
 }
