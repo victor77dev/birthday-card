5c2342df4bd55f3c23a11df5aabfb249b1236c31
victor77dev <victor77.dev@gmail.com>
2020-08-21 23:19:00 +0200

    Add GuideBase images

--- src/pages/guide-base.js
+++ src/pages/guide-base.js
@@ -12,6 +12,10 @@ export class GuideBase {
     element =
     '<div class="page">\
         <h1>Assemble 4 triangles base</h1>\
+        <div class="guide-container">\
+            <img src="images/base-1.jpg" class="base image" alt="Step 1" />\
+            <img src="images/base-2.jpg" class="base image" alt="Step 2" />\
+        </div>\
         <button id="next">Next</button>\
     </div>';
 }
