619b29b70617c208f483428dbc8061693c7ba68d
victor77dev <victor77.dev@gmail.com>
2020-08-24 00:39:22 +0200

    Add slideshow buttons

--- src/pages/check-parts.js
+++ src/pages/check-parts.js
@@ -7,10 +7,12 @@ export class CheckParts {
         '<div class="page">\
             <h1>Make sure you have all the parts!</h1>\
             <div class="parts-container">\
-                <img src="images/parts-1.jpg" class="parts image" alt="Four base" />\
+                <img src="images/parts-1.jpg" class="active parts image" alt="Four base" />\
                 <img src="images/parts-2.jpg" class="parts image" alt="Project & Star Moon" />\
                 <img src="images/parts-3.jpg" class="parts image" alt="Reflect card" />\
                 <img src="images/parts-4.jpg" class="parts image" alt="Phone card" />\
+                <button class="prev">❮</button>\
+                <button class="next">❯</button>\
             </div>\
             <button class="button" id="next">Next</button>\
         </div>';
