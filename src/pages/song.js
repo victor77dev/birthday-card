80f8e7c5461fef1d2472f36eb839fe5b52aa63e8
victor77dev <victor77.dev@gmail.com>
2020-08-29 22:48:21 +0200

    Use separate audio & video

--- src/pages/song.js
+++ src/pages/song.js
@@ -4,16 +4,30 @@ export class Song {
         '<div class="page">\
             <button id="close" class="hidden">close</button>\
             <video id="video" width="100%" playsinline controls></video>\
+            <audio id="audio"></audio>\
         </div>';
     }
 
     init() {
-        this.playVideo('happy-birthday-song-only');
+        this.loadVideo('happy-birthday-video');
+        this.loadAudio('birthday-song-english');
+        this.playAudioVideo();
     };
 
-    playVideo(song) {
-        const video = document.querySelector('#video');
-        video.src = `songs/${song}.mp4`;
-        video.play();
+    loadVideo(video) {
+        this.video = document.querySelector('#video');
+        this.video.src = `songs/${video}.mp4`;
+    }
+
+    loadAudio(song) {
+        this.audio = document.querySelector('#audio');
+        this.audio.src = `songs/${song}.mp3`;
+    }
+
+    playAudioVideo() {
+        setTimeout(() => {
+            this.audio.play();
+        }, 1000);
+        this.video.play();
     }
 }
--- src/pages/video.js
+++ src/pages/video.js
@@ -14,7 +14,7 @@ export class Video {
         this.addCloseButton();
         this.fullscreen(app);
         this.lockOrientation('portrait-primary');
-        this.startVideo('happy-birthday-song-only');
+        this.startVideo('happy-birthday-video');
 
         this.video = document.querySelector('#video');
     };
